986ddc67ae058a72f134a4e572c7eedac1e9bcab
kaushalRaut <121119528+kaushalRaut@users.noreply.github.com>
2022-12-21 09:17:45 +0545

    change setting

---
+++ database/migrations/2022_12_19_175001_create_customer_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->increments('customer_id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('country');
+            $table->string('E-mail');
+            $table->
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
